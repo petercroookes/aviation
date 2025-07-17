34d4d5aa6b6c1c32380f5763da985097edf985ab
Peter Crookes <pc693@cam.ac.uk>
2025-07-17 11:51:56 +0100

    Python files formatted using `ruff`

--- analysis/passengers_per_day.py
+++ analysis/passengers_per_day.py
@@ -5,6 +5,8 @@ days_per_yer = 365.25
 seats_per_aircraft = 181.0
 flights_per_aircraft_per_day = 4.0
 
-passengers_per_day = aviation.calculate_passengers_per_day(passengers_per_year=passengers_per_year, days_per_yer=days_per_yer)
+passengers_per_day = aviation.calculate_passengers_per_day(
+    passengers_per_year=passengers_per_year, days_per_yer=days_per_yer
+)
 
 print(f"Passengers Per Day = {passengers_per_day:.0f}")
