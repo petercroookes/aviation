ae8303bf0c8c439a9fd6522052251c9c0d39ca69
Peter Crookes <pc693@cam.ac.uk>
2025-07-17 09:58:10 +0100

    Refactor aviation.py to contain functions, and add two scripts calling them

--- aviation.py
+++ aviation.py
@@ -1,9 +1,5 @@
-passengers_per_year = 9_000_000_000.0
-days_per_yer = 365.25
-seats_per_aircraft = 181.0
-flights_per_aircraft_per_day = 4.0
+def calculate_passengers_per_day(passengers_per_year, days_per_yer):
+    return passengers_per_year / days_per_yer
 
-passengers_per_day = passengers_per_year/days_per_yer
-required_global_fleet = passengers_per_day / (seats_per_aircraft*flights_per_aircraft_per_day)
-
-print(f"Global Fleet Requirement = {required_global_fleet:.0f} Aircraft")
+def calculate_required_global_fleet(passengers_per_day, seats_per_aircraft, flights_per_aircraft_per_day):
+    return passengers_per_day / (seats_per_aircraft * flights_per_aircraft_per_day)    
