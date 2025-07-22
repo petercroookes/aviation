872bfc00a03c0caf0844b86302e6b983f630364d
Peter Crookes <pc693@cam.ac.uk>
2025-07-22 11:57:12 +0100

    Added doc strings to module and functions

--- src/aviation/__init__.py
+++ src/aviation/__init__.py
@@ -1,3 +1,10 @@
+"""A simple model of global aviation.
+
+Modules:
+    fleet: Modelling of the global fleet based on average passenger and aircraft data.
+
+"""
+
 __all__ = ("calculate_required_global_fleet", "calculate_passengers_per_day")
 
 from aviation.fleet import (
