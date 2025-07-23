67c1e78460f760ac8b94f83718c70a6e36746bf9
Peter Crookes <pc693@cam.ac.uk>
2025-07-23 12:07:45 +0100

    Deleted `_engine.py` and `_model.py`, refactoring for camia packages

--- analysis/passengers_per_day.py
+++ analysis/passengers_per_day.py
@@ -1,12 +1,16 @@
 """Analysis to determine the average number of passengers flying daily."""
 
+import camia_engine as engine
+
 import aviation
 
 passengers_per_year = 9_000_000_000.0
 days_per_year = 365.25
 
-passengers_per_day = aviation.passengers_per_day(
-    passengers_per_year=passengers_per_year, days_per_year=days_per_year
-)
+inputs = {"days_per_year": days_per_year, "passengers_per_year": passengers_per_year}
+output = "passengers_per_day"
+
+systems_model = engine.SystemsModel(aviation.transforms)
+passengers_per_day = systems_model.evaluate(inputs, output)
 
 print(f"Passengers Per Day = {passengers_per_day:.0f}")
