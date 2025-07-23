3bd9ade8b21233c633b5cf0d589e752ed9e91c62
Peter Crookes <pc693@cam.ac.uk>
2025-07-23 07:43:05 +0100

    Fixed isort-style error in `src/aviation/__init__.py`

--- src/aviation/__init__.py
+++ src/aviation/__init__.py
@@ -5,7 +5,7 @@ Modules:
 
 """
 
-__all__ = ("calculate_required_global_fleet", "calculate_passengers_per_day")
+__all__ = ("calculate_passengers_per_day", "calculate_required_global_fleet")
 
 from aviation.fleet import (
     calculate_passengers_per_day,
